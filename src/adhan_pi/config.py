9ae97a958c105d0a4bbcd47d6fad2b9443ec79d1
Salah Ahmed <salahs.email@pm.me>
2021-01-04 18:30:03 -0500

    rename automock config

--- src/adhan_pi/config.py
+++ src/adhan_pi/config.py
@@ -4,4 +4,4 @@ FAJR_ADHAN_MP3_PATH = os.getenv(
     "FAJR_ADHAN_MP3_PATH", "/opt/adhan-pi/static/azan-fajr.mp3"
 )
 ADHAN_MP3_PATH = os.getenv("ADHAN_MP3_PATH", "/opt/adhan-pi/static/azan2.mp3")
-AUTOMOCK_REGISTRATION_IMPORTS = ("adhan_pi.mocks",)
+AUTOMOCK_REGISTRATION_IMPORTS = ("adhan_pi.test_mocks",)
