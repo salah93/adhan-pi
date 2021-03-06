7bbd3300fef1bdda5644bfd2a9a5f698c3dae5ad
Salah Ahmed <salahs.email@pm.me>
2021-03-06 09:34:27 -0500

    update test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,7 +4,14 @@ import pytest
 import responses
 from automock import get_mock
 
+import adhan_pi
 import adhan_pi.utils as utils
+from adhan_pi.dataclasses import PrayerTimes
+
+
+@pytest.fixture(autouse=True)
+def prayer_times_cache_restart():
+    utils.get_prayer_times_for_month.cache_clear()
 
 
 def test_get_location():
@@ -20,10 +27,9 @@ def test_get_location():
 @responses.activate
 def test_get_prayer_times(prayer_api_200_response):
     location = utils.get_location_from_query("New York, NY")
-    p = utils.PrayertimesAPI()
     day = dt.date(year=2021, month=1, day=1)
-    prayer_times = p.get_prayer_times(location, day)
-    assert isinstance(prayer_times, utils.PrayerTimes)
+    prayer_times = adhan_pi.get_prayer_times(location, day)
+    assert isinstance(prayer_times, PrayerTimes)
     assert prayer_times.fajr.name == "fajr"
     assert prayer_times.fajr.time == dt.time(hour=4, minute=58)
     assert prayer_times.date == day
@@ -41,28 +47,25 @@ def test_get_prayer_times_200_bad_response(
     prayer_api_200_response_different_version_response,
 ):
     location = utils.get_location_from_query("New York, NY")
-    p = utils.PrayertimesAPI()
     day = dt.date(year=2021, month=1, day=1)
     with pytest.raises(utils.PrayerAPIError):
-        p.get_prayer_times(location, day)
+        adhan_pi.get_prayer_times(location, day)
 
 
 @responses.activate
 def test_get_prayer_times_400(prayer_api_400_response):
     location = utils.get_location_from_query("New York, NY")
-    p = utils.PrayertimesAPI()
     day = dt.date(year=2021, month=1, day=1)
     with pytest.raises(utils.PrayerAPIError):
-        p.get_prayer_times(location, day)
+        adhan_pi.get_prayer_times(location, day)
 
 
 @responses.activate
 def test_get_prayer_times_correct_day(prayer_api_200_response):
     location = utils.get_location_from_query("New York, NY")
-    p = utils.PrayertimesAPI()
     # day marked as the second of january
     day = dt.date(year=2021, month=1, day=2)
-    prayer_times = p.get_prayer_times(location, day)
-    assert isinstance(prayer_times, utils.PrayerTimes)
+    prayer_times = adhan_pi.get_prayer_times(location, day)
+    assert isinstance(prayer_times, PrayerTimes)
     assert prayer_times.fajr.name == "fajr"
     assert prayer_times.fajr.time == dt.time(hour=4, minute=59)
